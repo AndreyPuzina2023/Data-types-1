9772d960c479215b29cf523d34b09cb6bafc4bea
AndreyP <puzinaandrey2023@gmail.com>
2023-10-25 12:01:18 +0300

    Initial kommit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 8956;
+        int miles = 20;
+        int bonus = ticketPrice / miles;
+        System.out.println("Количство бонусов составляет  " + bonus);
+    }
+}
